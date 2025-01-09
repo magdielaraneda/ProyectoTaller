17eada43bb38a9b163c7c17f1e715bc5f39cb059
Magdiel Araneda <aniiko.magdi@gmail.com>
2025-01-09 01:20:42 -0300

    Configuración dinámica de CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const allowedOrigins = [
-  "process.env.FRONTEND_URL",
+  "https://frontend-b5hjmqltg-magdielaranedas-projects.vercel.app",
   "http://localhost:5173",
 ];
 
